f8bdeac211cc53837a68961fd9e4a545845c0eb1
mmsesay <maej_01@hotmail.com>
2022-02-07 19:50:10 +0000

    navigation page after log in was successful

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,3 +1,9 @@
 class SplashController < ApplicationController
+  before_action :check_signed_in
+
   def index; end
+
+  def check_signed_in
+    redirect_to transactions_path if signed_in?
+  end
 end
