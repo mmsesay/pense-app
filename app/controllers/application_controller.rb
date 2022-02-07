1f35122f9af59027f7ec36a9e4aab3bcddda8d3f
mmsesay <maej_01@hotmail.com>
2022-02-07 20:27:44 +0000

    Update controllers ;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
-  # before_action :check_signed_in
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def check_signed_in
-  #   redirect_to home_path if signed_in?
-  # end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmaiton])
+  end
+
+  def check_signed_in
+    redirect_to transactions_path if signed_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
- 
+
   get '/transactions', to: 'entity#index'
 
   # Defines the root path route ("/")
