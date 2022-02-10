5f887b97254ad9f62df5fa39b8fe4b71071a01de
mmsesay <maej_01@hotmail.com>
2022-02-10 09:05:53 +0000

    Fix deleting for transactions

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,26 +1,42 @@
 // import "@hotwired/turbo-rails"
 
 import "controllers"
-const btn_open_popup = document.querySelector('.btn-open-popup');
-const btn_close_popup = document.getElementById('btn-close-popup');
-const popup_modal = document.querySelector('.popup-modal');
+const btn_category_trash_icon = document.querySelector('.btn-open-cate-popup');
+const popup_for_transaction = document.querySelector('.confirm-transaction-delete');
+const popup_for_category = document.querySelector('.confirm-category-delete');
 const notice_card = document.querySelector('.notice-card');
 
-
-if (btn_open_popup) {
-  btn_open_popup.addEventListener('click', () => {
+if (btn_category_trash_icon) {
+  btn_category_trash_icon.addEventListener('click', () => {
+    popup_for_category.classList.remove('hidden');
     document.body.style.overflow = 'hidden';
-    popup_modal.classList.remove('hidden');
   });
 }
 
-if (btn_close_popup) {
-  btn_close_popup.addEventListener('click', () => {
-    window.location.reload();
-    popup_modal.classList.add('hidden');
-    document.body.style.overflow = 'scroll';
+document.querySelectorAll('.btn-open-trans-popup').forEach((element) => {
+  element.addEventListener('click', () => {
+    popup_for_transaction.classList.remove('hidden');
+    document.body.style.overflow = 'hidden';
+
+    document.querySelectorAll('.delete-button').forEach((delete_button) => {
+      delete_button.id = element.id;
+      delete_button.addEventListener('click', (e) => {
+        fetch(`/transactions/${e.target.id}/delete`)
+          .then(() => { window.location.reload(); });
+      });
+    });
   });
-}
+});
+
+document.querySelectorAll('.btn-close-popup').forEach((element) => {
+  element.addEventListener('click', () => {
+    popup_for_category.classList.add('hidden');
+    popup_for_transaction.classList.add('hidden');
+    document.body.style.overflow = 'scroll';
+    window.location.reload();
+  });
+});
+
 
 // hide notice card when new notice enters
 if (typeof(notice_card) != 'undefined' && notice_card != null) {
